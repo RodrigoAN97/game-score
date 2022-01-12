3123504abffad17da7350b055470c8a3782d6970
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-01-12 16:40:56 -0300

    feat: :sparkles: nebular setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { ReadComponent } from './read/read.component';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { GameState } from './state/game.state';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbInputModule,
+  NbDatepickerModule,
+  NbButtonModule,
+  NbSelectModule,
+} from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, CreateComponent, ReadComponent],
@@ -15,6 +27,16 @@ import { GameState } from './state/game.state';
     NgxsModule.forRoot([GameState]),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot(),
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    AppRoutingModule,
+    NbInputModule,
+    NbDatepickerModule.forRoot(),
+    NbButtonModule,
+    NbSelectModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
