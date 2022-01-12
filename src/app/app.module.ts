c65137e041cf387a2104970371be074d08c6d7ae
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-01-12 11:57:35 -0300

    feat: :sparkles: setup ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { TutorialState } from './state/tutorial.state';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxsModule } from '@ngxs/store';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, CreateComponent, ReadComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxsModule.forRoot([TutorialState]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
