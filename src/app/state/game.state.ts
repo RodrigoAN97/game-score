457a8e6c1eaeaa826db0ced5168cc36e71bc1fcc
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-05-04 15:00:27 -0300

    feat: add games on firestore

--- src/app/state/game.state.ts
+++ src/app/state/game.state.ts
@@ -1,4 +1,7 @@
+import { Injectable } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
 import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { addDoc, collection } from 'firebase/firestore';
 import { AddGame, RemoveGame } from '../actions/game.actions';
 import { IGame } from '../models/game.models';
 
@@ -12,17 +15,23 @@ export class GameStateModel {
     games: [],
   },
 })
+
+@Injectable()
 export class GameState {
+  constructor(private firestore: Firestore) {}
+
   @Selector()
   static getGames(state: GameStateModel) {
     return state.games;
   }
 
   @Action(AddGame)
-  add(
+  async add(
     { getState, patchState }: StateContext<GameStateModel>,
     { payload }: AddGame
   ) {
+    await addDoc(collection(this.firestore, "games"), payload);
+    console.log(payload);
     const state = getState();
     patchState({ games: [...state.games, payload] });
   }
