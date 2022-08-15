5419b978388af531853c6dc5178ea1c16a65d39c
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-08-15 15:49:24 -0300

    don't allow to choose the same player

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -35,8 +35,16 @@ export class CreateComponent implements OnInit {
 
   addGame() {
     const docId = uid(21);
-    const players = [this.gameForm.value.player1, this.gameForm.value.player2].sort((a,b) => a < b ? -1 : 1);
-    const docData :IGame = {
+    const player1 = this.gameForm.value.player1;
+    const player2 = this.gameForm.value.player2;
+    const players = [player1, player2].sort((a, b) => (a < b ? -1 : 1));
+
+    if (player1 === player2) {
+      alert('You need to choose different players');
+      return;
+    }
+
+    const docData: IGame = {
       players,
       winner: this.gameForm.value.winner,
       id: docId,
