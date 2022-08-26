991d43bf8a6d429161e733d89b2f903706f77ab7
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-08-26 16:50:42 -0300

    import from angular fire

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,26 +1,30 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { collectionData, Firestore } from '@angular/fire/firestore';
-import { collection, deleteDoc, doc, setDoc } from 'firebase/firestore';
-
+import {
+  collectionData,
+  Firestore,
+  collection,
+  deleteDoc,
+  doc,
+  setDoc,
+} from '@angular/fire/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirestoreService {
+  constructor(private firestore: Firestore) {}
 
-  constructor(private firestore: Firestore) { }
-
-  async setDocument(collectionName:string, docId:string, docData:any) {
+  async setDocument(collectionName: string, docId: string, docData: any) {
     const collectionRef = collection(this.firestore, collectionName);
-    await setDoc(doc(collectionRef, docId), docData);  
+    await setDoc(doc(collectionRef, docId), docData);
   }
 
   async deleteDocument(collectionName: string, docId: string) {
     await deleteDoc(doc(this.firestore, collectionName, docId));
   }
 
-  getCollection (collectionName:string): Observable<any[]> {
+  getCollection(collectionName: string): Observable<any[]> {
     const col = collection(this.firestore, collectionName);
     return collectionData(col);
   }
