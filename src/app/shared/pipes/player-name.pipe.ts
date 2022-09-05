08108dba317fb14e8594f778dd76c63c0b3c10f7
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-09-05 17:48:58 -0300

    type

--- src/app/shared/pipes/player-name.pipe.ts
+++ src/app/shared/pipes/player-name.pipe.ts
@@ -1,21 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { from } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Pipe({
-  name: 'playerName'
+  name: 'playerName',
 })
 export class PlayerNamePipe implements PipeTransform {
-  constructor(private firestoreService: FirestoreService){}
+  constructor(private firestoreService: FirestoreService) {}
 
-  transform(playerId: string) {
+  transform(playerId: string): Observable<string | null> {
     const playerName = from(this.playerName(playerId));
     return playerName;
   }
 
-  async playerName(playerId: string){
+  async playerName(playerId: string): Promise<string | null> {
     const name = (await this.firestoreService.getUser(playerId)).displayName;
     return name;
   }
-
 }
