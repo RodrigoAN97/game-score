5a36c4a0c48f1140b554bf6c3456b6af1b6b71b6
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-09-14 19:29:24 -0300

    own user also

--- src/app/shared/pipes/permission-to-create.pipe.ts
+++ src/app/shared/pipes/permission-to-create.pipe.ts
@@ -3,14 +3,17 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { DBUser } from '../interfaces';
 
 @Pipe({
-  name: 'permissionToCreate'
+  name: 'permissionToCreate',
 })
 export class PermissionToCreatePipe implements PipeTransform {
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
   transform(players: DBUser[] | null): DBUser[] | undefined {
     const userUid = this.authService.userUid as string;
-    return players?.filter(player => player.permittedUsers?.includes(userUid));
+    console.log({userUid});
+    return players?.filter(
+      (player) =>
+        player.permittedUsers?.includes(userUid) || player.uid === userUid
+    );
   }
-
 }
